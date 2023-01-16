838c59a28daee500822adb68e620ad8ad652e1ac
terumitt <terumitt@pepabo.com>
2023-01-16 11:35:51 +0900

    数値に変換

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -10,7 +10,19 @@ opt.parse!(ARGV)
 score = ARGV[0]
 # 取得した引数を1投毎に分割する
 score.split(',')
-# 数字に変換
+# 数値に変換
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+  if s == 'X' # strike
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
+p shots
 # 投球（フレーム）ごとに分割
 # 合計する
 # スペアとストライクを加算
