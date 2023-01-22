943e36bf401e7de0afff0b88460cb7701b062cc4
terumitt <f4k0m6r0@icloud.com>
2023-01-22 22:39:10 +0900

    コマンドラインの入力値から取得するdateを判別するif文の冗長を改善

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,6 +2,42 @@
 require 'date'
 require 'optparse'
 
+# 指定した年月を文字列で表現
+def year_month_str(run_date)
+  # year_output = run_date.year.to_s
+  # month_output = run_date.month.to_s
+  output_string = run_date.month.to_s + "月" + "\s" + run_date.year.to_s
+end
+p year_month_str
+# 曜日を文字列として表現
+def weeks_str
+  "\s日\s月\s火\s水\s木\s金\s土"
+end
+p weeks_str
+# #指定した年月の日数の取得
+# def month_days(run_date)
+#   first_day = Date.new(@run_today.year, @run_today.month, 1)
+#   last_day = Date.new(run_date.year, run_date.month, -1)
+
+#   output_string = ""
+#   output_string += year_month_str.center(21) + "\n"
+#   output_string += weeks_str + "\n"
+  
+#   # #月頭の空白処理
+#   output_string += "".rjust(3) * first_day.wday
+
+#   # #取得した日数の月ごとの正規化
+#   # #日数の繰り返し
+#   (1..last_day).each do |day|
+#     if day.wday == 6
+#       output_string += day.day.to_s.rjust(3) + "\n"
+#     else
+#       output_string += day.day.to_s.rjust(3)
+#     end
+#   end
+#   output_string
+# end
+
 opt = OptionParser.new
 option = {}
 
@@ -10,56 +46,19 @@ opt.on('-m [v]') {|v| option[:m] = v}
 
 opt.parse!(ARGV)
 
-year = option[:y].to_i
-month = option[:m].to_i
-
+# コマンドラインで入力された年月の数値化
+year_num = option[:y].to_i
+month_num = option[:m].to_i
+# コマンドラインの入力がなかった時に現在の日にちをあてる
 today = Date.today
-
-if year > 0 && month > 0
-  @run_today = Date.new(year, month)
-elsif year > 0
-  @run_today = Date.new(year = year, mon = today.month)
-elsif month > 0
-  @run_today = Date.new(year = today.year, mon = month)
+# 入力値を使いdateを取得する
+if year_num > 0 
+  run_date = Date.new(year = year_num , month = today.month)
+elsif month_num > 0
+  run_date = Date.new(year = today.year, month = month_num)
 else
-  @run_today = Date.today
+  run_date = today
 end
-
-#指定した年月を取得
-def year_date
-  year_name = @run_today.year.to_s
-  month_name = @run_today.month.to_s
-  output_string = month_name + "月" + "\s" + year_name
-end
-
-#曜日の取得
-def weeks_name
-  "\s日\s月\s火\s水\s木\s金\s土"
-end
-
-#指定した年月の日数の取得
-def month_days
-  first_day = Date.new(@run_today.year, @run_today.month, 1)
-  last_day = Date.new(@run_today.year, @run_today.month, -1)
-
-  output_string = ""
-  output_string += year_date.center(21) + "\n"
-  output_string += weeks_name + "\n"
-  
-  # #月頭の空白処理
-  output_string += "".rjust(3) * first_day.wday
-
-  # #取得した日数の月ごとの正規化
-  # #日数の繰り返し
-  (first_day..last_day).each do |day|
-    if day.wday == 6
-      output_string += day.day.to_s.rjust(3) + "\n"
-    else
-      output_string += day.day.to_s.rjust(3)
-    end
-  end
-  output_string
-end
-
+p run_date
 # 正規化した日数の表示
-puts month_days
+# puts month_days
