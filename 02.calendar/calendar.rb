35d1b7673a49c0f43bfb5dec2a3b4e276c01dbf6
terumitt <f4k0m6r0@icloud.com>
2023-01-23 12:33:11 +0900

    年月の文字列表示の改善

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,31 +3,27 @@ require 'date'
 require 'optparse'
 
 # 指定した年月を文字列で表現
-def year_month_str(run_date)
-  # year_output = run_date.year.to_s
-  # month_output = run_date.month.to_s
-  output_string = run_date.month.to_s + "月" + "\s" + run_date.year.to_s
+def year_month_str(subject_date)
+  output_string = subject_date.month.to_s + "月" + "\s" + subject_date.year.to_s
 end
-p year_month_str
 # 曜日を文字列として表現
 def weeks_str
   "\s日\s月\s火\s水\s木\s金\s土"
 end
-p weeks_str
-# #指定した年月の日数の取得
-# def month_days(run_date)
-#   first_day = Date.new(@run_today.year, @run_today.month, 1)
-#   last_day = Date.new(run_date.year, run_date.month, -1)
+# # 指定した年月の日数の取得
+# def month_days(get_date)
+#   # first_day = Date.new(get_date.year, get_date.month, 1)
+#   last_day = Date.new(get_date.year, get_date.month, -1)
 
 #   output_string = ""
 #   output_string += year_month_str.center(21) + "\n"
 #   output_string += weeks_str + "\n"
   
-#   # #月頭の空白処理
+#   # 月頭の空白処理
 #   output_string += "".rjust(3) * first_day.wday
 
-#   # #取得した日数の月ごとの正規化
-#   # #日数の繰り返し
+#   # 取得した日数の月ごとの正規化
+#   # 日数の繰り返し
 #   (1..last_day).each do |day|
 #     if day.wday == 6
 #       output_string += day.day.to_s.rjust(3) + "\n"
@@ -60,5 +56,6 @@ else
   run_date = today
 end
 p run_date
+p year_month_str(run_date)
 # 正規化した日数の表示
-# puts month_days
+# puts month_days(run_date)
