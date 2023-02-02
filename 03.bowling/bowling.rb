c3b94c907fd10f8e7a423e03a009a203eeab919d
terumitt <f4k0m6r0@icloud.com>
2023-02-02 19:16:49 +0900

    if文で判断する条件の見直し・rubocopによるシバンのエラー対策

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
 #!/usr/bin/env ruby
+
 require 'optparse'
 
 # コマンドラインの入力値（引数）を取り出す
@@ -20,47 +22,40 @@ scores.each do |scores_string|
            end
 end
 # 投球（フレーム）ごとに分割
-frames = []
-(1..10).each do |frame_count|
-  frame_index = frame_count - 1
-  frames[frame_index] = if frame_count < 10
-                          if shots[0] == 10
-                            shots.slice!(0, 1)
-                          else
-                            shots.slice!(0, 2)
-                          end
-                        else
-                          shots
-                        end
+frames = (1..10).map do |frame_count|
+  if frame_count < 10
+    if shots[0] == 10
+      shots.slice!(0, 1)
+    else
+      shots.slice!(0, 2)
+    end
+  else
+    shots
+  end
 end
+
 # スコアの合計する
 # ストライクとスペアの判断
-point = 0
-frames.each_with_index do |frame, index|
-  if index > 8
-    point += frame.sum
-    next
-  end
+point = frames.each_with_index.sum do |frame, index|
+  next frame.sum if index > 8
 
-  if index <= 8
-    # ストライクなら次のフレームの1・2投目を足す
-    if frame[0] == 10 # strike
-      next_frame = frames[index + 1]
-      # 連続ストライクなら次のフレームと次のフレーム次のを足す
-      if next_frame == [10]
-        after_next_frame = frames[index + 2]
-        point += (frame.sum + next_frame.sum + after_next_frame.first)
-      else
-        next_frame_score = next_frame.first(2)
-        point += (frame.sum + next_frame_score.sum)
-      end
-    # スペアなら次のフレームの1投目の足す
-    elsif frame.sum == 10 # spare
-      next_frame = frames[index + 1]
-      point += (frame.sum + next_frame.first)
+  # ストライクなら次のフレームの1・2投目を足す
+  if frame[0] == 10 # strike
+    next_frame = frames[index + 1]
+    # 連続ストライクなら次のフレームと次のフレーム次のを足す
+    if next_frame == [10]
+      after_next_frame = frames[index + 2]
+      frame.sum + next_frame.sum + after_next_frame.first
     else
-      point += frame.sum
+      next_frame_score = next_frame.first(2)
+      frame.sum + next_frame_score.sum
     end
+  # スペアなら次のフレームの1投目の足す
+  elsif frame.sum == 10 # spare
+    next_frame = frames[index + 1]
+    frame.sum + next_frame.first
+  else
+    frame.sum
   end
 end
 puts point
