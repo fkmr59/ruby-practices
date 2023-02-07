beba867f81f647dbf09bb7fdd60a26cc13c91c4d
terumitt <f4k0m6r0@icloud.com>
2023-02-07 16:33:53 +0900

    一旦完成？

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,58 +1,43 @@
 # frozen_string_literal: true
 #!/user/bin/env ruby
 
-# 要素一つ一つにスペースを入れたい
-# 出力に入る要素は横方向に一つの配列
-# 要素の見た目は縦方向に揃える
-
-def one_row_items(dir, hope_roundup_columns)
+def output_one_row_items(dir, hope_roundup_columns)
   items_arrays_by_column = []
-  dir.each_slice(hope_roundup_columns) do |b|
-    items_arrays_by_column << b
+  dir.each_slice(hope_roundup_columns) do |cut_items|
+    items_arrays_by_column << cut_items
   end
+  
+  rebuild_repeat = items_arrays_by_column[0].length
 
+  index= 0
   items_arrays_by_row = []
-  items_arrays_by_column.each do |items_array|
-    if items_arrays_by_row[0].nil?
-      items_arrays_by_row[0] = []
+  rebuild_repeat.times do
+    items_arrays_by_column.each do |items_array|
+      if items_arrays_by_row[index].nil?
+        items_arrays_by_row[index] = []
+      end
+      items_arrays_by_row[index] << items_array[index]
+      p items_arrays_by_row
     end
-    items_arrays_by_row[0] << items_array[0]
+    index += 1
   end
-  items_arrays_by_column.each do |items_array|
-    if items_arrays_by_row[1].nil?
-      items_arrays_by_row[1] = []
-    end
-    items_arrays_by_row[1] << items_array[1]
-  end
-  items_arrays_by_column.each do |items_array|
-    if items_arrays_by_row[2].nil?
-      items_arrays_by_row[2] = []
-    end
-    items_arrays_by_row[2] << items_array[2]
-  end
-  items_arrays_by_column.each do |items_array|
-    if items_arrays_by_row[3].nil?
-      items_arrays_by_row[3] = []
-    end
-    items_arrays_by_row[3] << items_array[3]
-  end
+  
   items_arrays_by_row.each do |array|
+    array.compact!
     output = ""
     array.each do |str|
-      output += str.to_s.ljust(10)
+      str_length = str.length + 3
+      output += str.to_s.ljust(str_length)
     end
     puts output
   end
 end
 
-
-
 # ディレクトリに存在するファイルの取得
 dir = Dir.glob("*")
 # 列の指定(num_of_columnsの数値だけを希望する列数に変える)
-hope_num_columns = 3
-floor_num = dir.length / hope_num_columns.to_f
-hope_roundup_columns = floor_num.ceil
-
-one_row_items(dir, hope_roundup_columns)
-
+hope_num_columns = 6
+Floor_num = dir.length / hope_num_columns.to_f
+hope_roundup_columns = Floor_num.ceil.freeze
+# 1行に出力する要素の数
+output_one_row_items(dir, hope_roundup_columns)
