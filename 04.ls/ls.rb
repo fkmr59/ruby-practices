15ea5d804686c4c5c8efb7035557700969a73b83
terumitt <f4k0m6r0@icloud.com>
2023-02-07 14:25:27 +0900

    列を指定する変数の追記

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,9 +5,9 @@
 # 出力に入る要素は横方向に一つの配列
 # 要素の見た目は縦方向に揃える
 
-def test2(dir)
+def one_row_items(dir, hope_roundup_columns)
   items_arrays_by_column = []
-  dir.each_slice(4) do |b|
+  dir.each_slice(hope_roundup_columns) do |b|
     items_arrays_by_column << b
   end
 
@@ -45,7 +45,14 @@ def test2(dir)
   end
 end
 
+
+
 # ディレクトリに存在するファイルの取得
 dir = Dir.glob("*")
-test2(dir)
+# 列の指定(num_of_columnsの数値だけを希望する列数に変える)
+hope_num_columns = 3
+floor_num = dir.length / hope_num_columns.to_f
+hope_roundup_columns = floor_num.ceil
+
+one_row_items(dir, hope_roundup_columns)
 
