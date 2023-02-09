e0c195a12a33771b25934d2f62ada647b93dfc14
terumitt <f4k0m6r0@icloud.com>
2023-02-09 12:40:09 +0900

    完成（気になるところは多々あるけど）

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,40 +1,38 @@
+# !/user/bin/env ruby
 # frozen_string_literal: true
-#!/user/bin/env ruby
 
 def output_one_row_items(dir, hope_num_columns, surplus)
   items_arrays_by_column = []
   cut_items = dir.length / hope_num_columns
-    if surplus > 0
-      surplus.times do
-        array_items = dir.slice!(0..cut_items)
-        items_arrays_by_column << array_items
-      end
-      dir.each_slice(cut_items) do |array_items|
-        items_arrays_by_column << array_items
-      end
-    else
-      dir.each_slice(cut_items) do |array_items|
-        items_arrays_by_column << array_items
-      end
+  if surplus > 0
+    surplus.times do
+      array_items = dir.slice!(0..cut_items)
+      items_arrays_by_column << array_items
     end
+    dir.each_slice(cut_items) do |array_items|
+      items_arrays_by_column << array_items
+    end
+  else
+    dir.each_slice(cut_items) do |array_items|
+      items_arrays_by_column << array_items
+    end
+  end
 
   rebuild_repeat = items_arrays_by_column[0].length
 
-  index= 0
+  index = 0
   items_arrays_by_row = []
   rebuild_repeat.times do
     items_arrays_by_column.each do |items_array|
-      if items_arrays_by_row[index].nil?
-        items_arrays_by_row[index] = []
-      end
+      items_arrays_by_row[index] = [] if items_arrays_by_row[index].nil?
       items_arrays_by_row[index] << items_array[index]
     end
     index += 1
   end
-  
+
   items_arrays_by_row.each do |array|
     array.compact!
-    output = ""
+    output = ''
     array.each do |str|
       str_length = str.length + 3
       output += str.to_s.ljust(str_length)
@@ -44,7 +42,7 @@ def output_one_row_items(dir, hope_num_columns, surplus)
 end
 
 # ディレクトリに存在するファイルの取得
-dir = Dir.glob("*")
+dir = Dir.glob('*')
 # 列の指定(hope_num_columnsの数値だけを希望する列数に変える)
 hope_num_columns = 5
 surplus = dir.length % hope_num_columns
