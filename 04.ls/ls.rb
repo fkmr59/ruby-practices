88cc43af203b4a2bbded461b32417202a5586963
terumitt <f4k0m6r0@icloud.com>
2023-02-13 18:10:55 +0900

    メソッド切り分け

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,7 +1,7 @@
 # !/user/bin/env ruby
 # frozen_string_literal: true
 
-def output_one_row_items(dir, hope_num_columns, surplus)
+def output_one_column_items(dir, hope_num_columns, surplus)
   items_arrays_by_column = []
   cut_items = dir.length / hope_num_columns
   if surplus.positive?
@@ -13,20 +13,25 @@ def output_one_row_items(dir, hope_num_columns, surplus)
   dir.each_slice(cut_items) do |array_items|
     items_arrays_by_column << array_items
   end
+  items_arrays_by_column
+end
 
-  rebuild_repeat = items_arrays_by_column[0].length
-
+def output_one_row_items(insert_column_items)
+  rebuild_repeat = insert_column_items[0].length
   index = 0
   items_arrays_by_row = []
   rebuild_repeat.times do
-    items_arrays_by_column.each do |items_array|
+    insert_column_items.each do |items_array|
       items_arrays_by_row[index] = [] if items_arrays_by_row[index].nil?
       items_arrays_by_row[index] << items_array[index]
     end
     index += 1
   end
+  items_arrays_by_row
+end
 
-  items_arrays_by_row.each do |array|
+def space_process(insert_row_items)
+  insert_row_items.each do |array|
     array.compact!
     output = ''
     array.each do |str|
@@ -43,5 +48,9 @@ dir = Dir.glob('*')
 hope_num_columns = 10
 surplus = dir.length % hope_num_columns
 
-# 1行に出力する要素の数
-output_one_row_items(dir, hope_num_columns, surplus)
+# 1列の要素数
+insert_column_items = output_one_column_items(dir, hope_num_columns, surplus)
+# 1行の要素数
+insert_row_items = output_one_row_items(insert_column_items)
+# 空白処理
+space_process(insert_row_items)
