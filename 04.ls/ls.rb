461ab5f3d37d9151eb2c994a4e23a537dae7b6e0
terumitt <f4k0m6r0@icloud.com>
2023-03-16 19:02:48 +0900

    一応空白の処理を完了

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
-def space_add(file__name_array)
-  ary = []
-  max_length = file__name_array.max.ljust(file__name_array.max.length + 1)
-  file__name_array.each do |item|
-    ary << item.ljust(max_length.length) if item.length < max_length.length
+def space_file_array(file_array)
+  space_file_array = []
+  initial_value = 0
+  file_array.each do |item|
+    string_length = item.length
+    initial_value = string_length if initial_value < string_length
   end
-  ary
+  file_array.each do |item|
+    space_file_array << item.ljust(initial_value + 1)
+  end
+  space_file_array
 end
 
-def one_column_items(blank_process)
+def one_column_items(items)
   column_count = 3
-  one_column_items = blank_process.each_slice(blank_process.length.div(column_count) + 1).to_a
+  one_column_items = items.each_slice(items.length.div(column_count) + 1).to_a
   if one_column_items.last.length == column_count
     one_column_items.transpose
   else
@@ -25,16 +29,19 @@ end
 def output_row_items(columns)
   columns.transpose.each do |row|
     row.compact!
-    p row
+    output = ''
+    row.each do |item|
+      output += item
+    end
+    puts output
   end
 end
 
-
 # ディレクトリに存在するファイルの取得
-file__name_array = Dir.glob('*')
+file_array = Dir.glob('*')
 # 空白処理
-blank_process = space_add(file__name_array)
-# 1列の要素数
-columns = one_column_items(blank_process)
-# 1行の要素数を出力
+items = space_file_array(file_array)
+# # 1列の要素数
+columns = one_column_items(items)
+# # 1行の要素数を出力
 output_row_items(columns)
